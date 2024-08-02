0525413de5ab92495b5b47dd3f7f54cdf200a08b
Rei Fidenccio <82601106+rei-fidenccio@users.noreply.github.com>
2024-08-02 05:46:56 -0300

    add jump to the player moviment prototype

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,17 +8,43 @@ public class Player : MonoBehaviour
 
     public float speed = 5f;
 
+    public float jumpForce = 2f;
+
+    public Vector2 friction = new Vector2(0.1f, 0);
+
     void Update()
+    {
+            
+        handleMoviment();
+        handleJump();
+    }
+
+    private void handleMoviment()
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.velocity = new Vector2(speed, rb.velocity.y);
         }
-        
-        if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.velocity = new Vector2(-speed, rb.velocity.y);
-        }     
+        }
 
+        if (rb.velocity.x > 0)
+        {
+            rb.velocity -= friction;
+        }
+        else if (rb.velocity.x < 0)
+        {
+            rb.velocity += friction;
+        }
+    }
+
+    private void handleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
     }
 }
