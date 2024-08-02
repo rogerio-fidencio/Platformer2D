e505280d432a52882b7e7b56032bf9d9bc93a23a
Rei Fidenccio <82601106+rei-fidenccio@users.noreply.github.com>
2024-08-02 10:00:42 -0300

    add jump animation to the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
 
+    [Header("Speed Setup")]
     public float walkSpeed = 5f;
-
     public float runSpeed = 10f;
-
     public float jumpForce = 2f;
-
-    public float _currentSpeed;
-
     public Vector2 friction = new Vector2(0.1f, 0);
+
+    [Header("Animation Setup")]
+    public float jumpScaleX = 0.5f;
+    public float jumpScaleY = 1.5f;
+    public float landingScaleX = 1.5f;
+    public float landingScaleY = 0.5f;
+    public float animationDuration = 0.3f;
+
+    private float _currentSpeed;
 
     void Update()
     {
@@ -62,6 +68,21 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.transform.localScale = Vector2.one;
+            DOTween.Kill(rb.transform);
+            handleAnimation();
         }
+    }
+
+    private void handleAnimation()
+    {
+        rb.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo);
+        rb.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.transform.DOScaleY(landingScaleY, animationDuration).SetLoops(2, LoopType.Yoyo);
+        rb.transform.DOScaleX(landingScaleX, animationDuration).SetLoops(2, LoopType.Yoyo);
     }
 }
